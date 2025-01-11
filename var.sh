526f967249c2d96b82c45bd267b2bf4ca0f94cb5
ahmed-kamal91 <nagmstar91@gmail.com>
2025-01-12 00:12:04 +0200

    update variables

--- var.sh
+++ var.sh
@@ -1,10 +1,16 @@
-# Main dire of all dbs
+# Main dir of all dbs 
 MainDIR="/home/$USER/databases"
 
-# for current database path
-current_DB_path="/home/ahmed"
+# Current DB path
+current_DB_path="$MainDIR/school"
 
-#for the name of the current database
+# Name of the current DB
 db_name="school"
+
+#for colors
+GRAY='\033[1;30m'
+GREEN='\033[0;32m'
+WHITE='\033[0;37m'
+RED='\033[0;31m'
 
 
