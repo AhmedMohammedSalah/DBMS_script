7d8c619c2d60514199ba041cfd85e29a3e14fda9
ahmed-kamal91 <nagmstar91@gmail.com>
2025-01-13 05:03:25 +0200

    IMP UPDATE

--- var.sh
+++ var.sh
@@ -13,11 +13,15 @@ tb_name="student"
 # Current Table path
 current_TB_path="$current_DB_path/$tb_name"
 
+# Current meta Table path
+current_meta_TB_path="$current_DB_path/.$tb_name"
+
 
 #for colors
-GRAY='\033[1;30m'
-GREEN='\033[0;32m'
-WHITE='\033[0;37m'
-RED='\033[0;31m'
+GRAY='\033[1;30m'  # for more info before enter
+GREEN='\033[0;32m' # for success
+WHITE='\033[0;37m' # normal enter
+RED='\033[0;31m'   #for error
+BLUE='\033[4;34m'  # for asking (y\n)
 
 
